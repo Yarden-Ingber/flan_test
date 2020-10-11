7f24e8e15e7083521abc6bd549a6ccfd24ea347f
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2020-10-11 20:23:34 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 set -e
 
 export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export BUILD_OUTPUT=$WORKDIR/build.out
+export BUILD_OUTPUT=build.out
 
 touch $BUILD_OUTPUT
 
